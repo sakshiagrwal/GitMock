9be7b8486f581595e222f12c4170656cd209b4d8
sakshiagrwal <81718060+sakshiagrwal@users.noreply.github.com>
2022-12-20 08:20:28 +0000

    🧹 Cleanup
    Signed-off-by: Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>

--- main.py
+++ main.py
@@ -5,13 +5,9 @@ from git import Repo
 
 FILE_PATH = './data.json'
 
+repo = Repo('./')
 
-def make_commit(n):
-    if n == 0:
-        repo = Repo('./')
-        repo.git.push()
-        return
-
+for i in range(20):
     x = random.randint(0, 54)
     y = random.randint(0, 6)
     date = (datetime.now() - timedelta(days=365)) + \
@@ -22,11 +18,8 @@ def make_commit(n):
     print(date.strftime('%Y-%m-%d'))
     with open(FILE_PATH, 'w') as f:
         json.dump(data, f)
-    repo = Repo('./')
     repo.git.add([FILE_PATH])
     repo.git.commit('-m', date.strftime('%Y-%m-%d'), '--date',
                     date.strftime('%Y-%m-%d %H:%M:%S'))
-    make_commit(n - 1)
 
-
-make_commit(2000)
+repo.git.push()
