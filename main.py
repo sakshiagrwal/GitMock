ac3251a8d59e0b5d89283f3b0b1b5a392e2f0ea9
Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>
2022-12-23 18:58:24 +0530

    🤷‍ Emoji-fying commits [1] Hardcode a list of emojis
    Signed-off-by: Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import random
+import emoji
+import emot
 from datetime import datetime, timedelta
 from git import Repo
 
@@ -15,17 +17,26 @@ for i in range(1):
     y = random.randint(0, 6)
     date = (datetime.now() - timedelta(days=365)) + \
         timedelta(days=1 + x * 7 + y)
+
+    # Use the random.choice() function to select a random emoji from a hardcoded list
+    emojis_list = ['😂', '❤', '💥', '❄️', '⛄', '⚡', '🌀', '😴', '🌌', '🚣', '⚓', '🚀',
+                   '😎', '🎉', '🎊', '🎈', '🔮', '📷', '📹', '🎥', '💻', '👾', '🎮', '💣']
+    emoji = random.choice(emojis_list)
+
     # Read the contents of the README.md file
     with open(FILE_PATH, 'r') as f:
         lines = f.readlines()
     # Modify the first line
-    lines[0] = f'# Random Commit Generated on: {date.strftime("%d/%m/%y")}\n'
+    lines[0] = '# Random Commit Generated on: {}\n'.format(
+        date.strftime("%d/%m/%y"))
     # Write the updated content to the file
     with open(FILE_PATH, 'w') as f:
         f.write(''.join(lines))
+    # Generate a commit message with the random emoji and commit date
+    commit_message = '{} {}'.format(emoji, date.strftime("%d/%m/%y"))
     # Add the file to the staging area and commit it
     repo.git.add([FILE_PATH])
-    repo.git.commit('-s', '-m', date.strftime('%Y-%m-%d'), '--date',
+    repo.git.commit('-s', '-m', commit_message, '--date',
                     date.strftime('%Y-%m-%d %H:%M:%S'))
 
 # Push the commits to the remote repository
