ee18aea692a18ea05408d2ed7bf7be7845c53409
Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>
2022-12-24 17:58:44 +0530

    Comments added
    Signed-off-by: Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>

--- script.js
+++ script.js
@@ -4,28 +4,41 @@ const moment = require("moment");
 const crypto = require("crypto");
 const FILE_PATH = "./data.json";
 
+/**
+ * Generates mock Git commits with random dates.
+ * @param {number} n - The number of commits to generate.
+ */
 async function makeCommit(n) {
+  // If no more commits are needed, push to the repository
   if (n === 0) return simpleGit().push();
 
+  // Generate random values for the number of weeks and days to add to the date
   const x = Math.floor(crypto.randomBytes(4).readUInt32BE() % 55);
   const y = Math.floor(crypto.randomBytes(4).readUInt32BE() % 7);
+
+  // Generate a random date within the past year
   const DATE = moment()
     .subtract(1, "y")
     .add(1, "d")
     .add(x, "w")
     .add(y, "d")
     .format();
+
+  // Write the date to a JSON file
   const data = {
     date: DATE,
   };
   console.log(DATE);
   try {
     await jsonfile.writeFile(FILE_PATH, data);
+    // Add and commit the file with the random date
     await simpleGit().add([FILE_PATH]).commit(`${DATE}`, { "--date": DATE });
+    // Recursively generate more commits
     makeCommit(--n);
   } catch (error) {
     console.error(`Error making commit: ${error}`);
   }
 }
 
+// Generate 20 mock commits
 makeCommit(20);
