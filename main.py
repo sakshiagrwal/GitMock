5f58d7a8ec8c0512c3e2f2307364d97595c91620
Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>
2022-12-24 13:09:54 +0530

    🚀 Improving
    Signed-off-by: Sakshi Aggarwal <81718060+sakshiagrwal@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,7 +1,44 @@
+# Copyright (C) 2022  Sakshi Aggarwal
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import json
 import random
 from datetime import datetime, timedelta
-from git import Repo
+from git import GitCommandError, Repo
+
+
+def generate_random_date():
+    """Generate a random date within the past year."""
+    week_index = random.randint(0, 52)
+    day_index = random.randint(0, 6)
+    date = (datetime.now() - timedelta(days=365)) + \
+        timedelta(days=1 + week_index * 7 + day_index)
+    return date
+
+
+def write_date_to_file(date, file_path):
+    """Write the given date to the specified data file."""
+    data = {
+        'date': date.strftime('%Y-%m-%d')
+    }
+    try:
+        with open(file_path, 'w') as f:
+            json.dump(data, f)
+    except IOError as e:
+        print(f'Error writing to file: {e}')
+
 
 # File path for the data file
 FILE_PATH = './data.json'
@@ -9,24 +46,21 @@ FILE_PATH = './data.json'
 # Initialize a Repo object for the current directory
 repo = Repo('./')
 
-# Make 100 commits to the current branch
-for i in range(100):
+# Make 1000 commits to the current branch
+for i in range(1000):
     # Generate a random date within the past year
-    x = random.randint(0, 54)
-    y = random.randint(0, 6)
-    date = (datetime.now() - timedelta(days=365)) + \
-        timedelta(days=1 + x * 7 + y)
+    date = generate_random_date()
     # Write the date to the data file
-    data = {
-        'date': date.strftime('%Y-%m-%d')
-    }
-    print(date.strftime('%Y-%m-%d'))
-    with open(FILE_PATH, 'w') as f:
-        json.dump(data, f)
+    write_date_to_file(date, FILE_PATH)
     # Add the file to the staging area and commit it
     repo.git.add([FILE_PATH])
     repo.git.commit('-m', date.strftime('%Y-%m-%d'), '--date',
                     date.strftime('%Y-%m-%d %H:%M:%S'))
 
-# Push the commits to the remote repository
-repo.git.push()
+    # Try to push the commits to the remote repository
+    try:
+        print(repo.git.push(force=True))
+    except GitCommandError as e:
+        # Print the error message and exit the loop
+        print(f'Error pushing commits: {e}')
+        break
